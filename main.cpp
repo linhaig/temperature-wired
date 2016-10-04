5dc81513cebb94c0cf68e328172374bd86e7e2b0
switches <switches@localhost>
2016-10-04 22:27:34 +0000

    New program to demonstrate PMIC on MAX32620HSP (MAXREFDES100#)

--- main.cpp
+++ main.cpp
@@ -1,12 +1,36 @@
 #include "mbed.h"
+#include "MAX14720.h"
 
-DigitalOut myled(LED1);
+// I2C Master 2
+I2C i2c2(I2C2_SDA, I2C2_SCL);
 
-int main() {
+#define  I2C_ADDR_PMIC   (0x54)
+MAX14720 max14720(&i2c2,I2C_ADDR_PMIC);
+
+DigitalOut led(LED1);
+InterruptIn button(SW1);
+
+void turnOff()
+{
+    max14720.shutdown();
+}
+
+int main()
+{
+    int result;
+    button.fall(&turnOff);
+    led = 0;
+    max14720.boostEn = MAX14720::BOOST_ENABLED;
+    result = max14720.init();
+    if (result == MAX14720_ERROR) printf("Error initializing MAX14720");
+    wait(1);
     while(1) {
-        myled = 1;
-        wait(0.2);
-        myled = 0;
-        wait(0.2);
+        max14720.boostSetMode(MAX14720::BOOST_DISABLED);
+        max14720.boostEn = MAX14720::BOOST_ENABLED;
+        wait(0.5);
+        max14720.boostSetVoltage(2500);
+        wait(0.5);
+        max14720.boostSetVoltage(5000);
+        wait(0.5);
     }
 }
